02ed39db7c9452a190b8e451bb5cbf3ea5bd2772
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-17 21:47:03 +0700

    function in class, refactor with es6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,17 @@ import React, {Component} from 'react'
 
 class SearchBar extends Component{
   render() {
-    return <input />
+    return <input onChange={ event => console.log(event.target.value) } /> 
   }
+
+  /* 
+  from above is es6 arrow, make function shorter.
+    (event) => console.log(event.target.value) 
+  equal =>
+    onInputChange(event) {
+      console.log(event.target.value)
+    } 
+  */
 }
 
 export default SearchBar;
