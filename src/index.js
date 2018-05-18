8760cee9b2622ac0b075bb78f2432e91eb37711e
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-18 19:42:06 +0700

    youtube-api static search and show data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
+
 
 const API_KEY = 'AIzaSyARO7zct2lmuqgK1Sy0_kFjEVRsNZ4HQlQ';
-
+YTSearch({key : API_KEY, term : 'surfboards'}, function(data){
+  console.log(data)
+});
 
 // step1 : create new component , declare it and return html block
 
