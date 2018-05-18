3d363a3a4fb28c13b911a39f6d07155b2b9d6107
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-18 14:58:30 +0700

    declare state, setState, show state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react'
 // {Component} equal => const Component = React.Component
 
 class SearchBar extends Component{
+  
+  constructor(props){
+    super(props)
+
+    this.state = { term: '' }
+  }
+
   render() {
-    return <input onChange={ event => console.log(event.target.value) } /> 
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value }) }/>
+        this value is {this.state.term}
+      </div>
+    )
   }
 
   /* 
