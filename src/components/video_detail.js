dca41c3405bb4c588d13af46d3616c2cd69abb59
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-19 11:41:39 +0700

    show video and detail.

---
+++ src/components/video_detail.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const VideoDetail  = ({video}) => {
+  if (!video){
+    return <div>Loading...</div>
+  }
+  const videoId = video.id.videoId;
+  const title = video.snippet.title;
+  const description = video.snippet.description;
+  const url = `https://www.youtube.com/embed/${videoId}`; 
+  /* es6 concat with string refactor from  =>   
+  const url = 'https://www.youtube.com/embed/' + videoId; 
+  */
+
+  return (
+  <div className="col-md-8">
+    <div className="embed-responsive embed-responsive-16by9">
+      <iframe className="embed-responsive-item" src={url} />
+    </div>
+    <div className="details">
+      <h4>{title}</h4>
+      <p>{description}</p>
+    </div>
+  </div>
+  );
+}
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
@@ -26,6 +27,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={ this.state.videos[0] } />
         <VideoList videos={ this.state.videos }/>
       </div>
     );
