943d69d3a7c3419d20e6097fc4ec82a339915669
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-05-19 10:14:47 +0700

    put data to UI, with bootstrap.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-   return (
-     <div>
-       {props.videoItem.snippet.channelTitle}
-    </div>
-   )
+const VideoItem = ({videoItem}) => {
+  /* ({videoItem}) equal =>  const videoItem = props.videoItem; */ 
+  const imageUrl = videoItem.snippet.thumbnails.default.url;
+  const title = videoItem.snippet.channelTitle;
+  return (
+    <li className="list-group media">
+      <div className="media-left">
+      <img src={imageUrl} alt= {title}/>
+      </div>
+      <div className="media-body">
+        <div className="media-heading">
+          {title}
+        </div>
+      </div>
+    </li>
+  )
 };
 
 export default VideoItem;
